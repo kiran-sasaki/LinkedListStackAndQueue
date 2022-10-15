3730843097c20aea64e61ea1fe9a689aa9b685be
kiran-sasaki <kiransekar1111@gmail.com>
2022-10-15 15:51:57 +0530

    Added Function To Create Linked List Queue

--- Program.cs
+++ Program.cs
@@ -6,15 +6,21 @@
         {
             Console.WriteLine("Welcome To Stack And Queue Problem");
 
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Insert(70);
-            linkedListStack.Insert(30);
-            linkedListStack.Insert(56);
-            linkedListStack.Display();
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            //    LinkedListStack linkedListStack = new LinkedListStack();
+            //    linkedListStack.Insert(70);
+            //    linkedListStack.Insert(30);
+            //    linkedListStack.Insert(56);
+            //    linkedListStack.Display();
+            //    linkedListStack.Peek();
+            //    linkedListStack.Pop();
+            //    linkedListStack.IsEmpty();
+            //    linkedListStack.Display();
+
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
         }
     }
 }
